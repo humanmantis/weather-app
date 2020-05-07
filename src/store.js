f9d8981f84ac674401f8954587f117fa3a95417b
Max Floreskul <maksfloreskyl@gmail.com>
2020-05-07 20:55:07 +0300

    Fixed work without Redux DevTools

--- src/store.js
+++ src/store.js
@@ -11,8 +11,9 @@ export default (initialState) => {
     initialState,
     compose(
       applyMiddleware(sagaMiddleware),
-      window.__REDUX_DEVTOOLS_EXTENSION__ &&
-        window.__REDUX_DEVTOOLS_EXTENSION__()
+      window.__REDUX_DEVTOOLS_EXTENSION__
+        ? window.__REDUX_DEVTOOLS_EXTENSION__()
+        : (f) => f
     )
   );
   sagaMiddleware.run(rootSaga);
