5044a9f478b565ffcdab757545eb6f4b560d2631
Max Floreskul <maksfloreskyl@gmail.com>
2020-05-07 16:50:41 +0300

    Updates

---
+++ precache-manifest.b59f7efa9919ef396a02f8aa514074b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1e10beb194d08047a043a03fda7f438",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "6334c81c222f759a2ae9",
+    "url": "/weather-app/static/css/main.5ec4cc03.chunk.css"
+  },
+  {
+    "revision": "cfd8c3ce950ca77e2b2e",
+    "url": "/weather-app/static/js/2.d6278bbc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/weather-app/static/js/2.d6278bbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6334c81c222f759a2ae9",
+    "url": "/weather-app/static/js/main.747dcfef.chunk.js"
+  },
+  {
+    "revision": "b583aba1370634b5a5a2",
+    "url": "/weather-app/static/js/runtime-main.60cbc992.js"
+  }
+]);
